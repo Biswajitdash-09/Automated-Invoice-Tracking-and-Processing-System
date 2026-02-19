3a5bda47116f19f16aa3de192e3170a3c33d50ce
Biswajitdash-09 <biswajitdash929@gmail.com>
2026-02-19 19:11:26 +0530

    aa

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['pdf-parse', '@napi-rs/canvas'],
+  serverExternalPackages: ['pdf-parse', '@napi-rs/canvas', 'xlsx'],
   images: {
     remotePatterns: [
       {
